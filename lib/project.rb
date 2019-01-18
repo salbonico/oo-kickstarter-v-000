07f82e467467c41120aef3a761e36625bacb062e
Steve Albonico <salbonico@gmail.com>
2019-01-18 22:04:40 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,18 @@
 class Project
 
-attr_accessor :title
+attr_accessor :title, :backers
 
 def initialize(title)
 @title = title
+@backers = []
 end
+
+def add_backer(backer)
+@backers << backer
+
+
+end
+
+
 
 end
